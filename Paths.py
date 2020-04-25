cf5723dea5261c14811c22ab43459ec517b8118b
nidhijoshi123 <nidhi.nj.joshi@fau.de>
2020-04-25 08:27:27 +0200

    Update Paths.py

--- Paths.py
+++ Paths.py
@@ -5,11 +5,11 @@ Created on Wed Feb 27 17:24:51 2019
 @author: Nidhi
 """
 
-channel1 = 'C4M1.txt'
-channel2 = 'F4M1.txt'
-channel3 = 'O2M1.txt'
+channel1 = 'C4M1.txt'     #central channel
+channel2 = 'F4M1.txt'     #frontal channel
+channel3 = 'O2M1.txt'     #occipital channel
 labels = 'labels.txt'
-sourceDataFolder = '/cluster/nidhi/Sleep_data/'
+sourceDataFolder = '.../Sleep_data/'  #Path to your three channel data
 sep = '/'
-samplesPerLabel = 7680
-numberOfCLasses = 5
+samplesPerLabel = 7680    #30*256
+numberOfCLasses = 5       #Wake, REM, N1,N2,N3
